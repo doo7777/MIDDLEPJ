4fe079704f8052c70a48f54572fcc7c5e9053b20
kim <kim@JTW96>
2024-12-10 18:15:07 +0900

    ㄴㄴㄴ

--- midpro/src/main/java/DaoInterface/IProductDao.java
+++ midpro/src/main/java/DaoInterface/IProductDao.java
@@ -5,13 +5,20 @@ import java.util.List;
 import Vo.ProductVO;
 
 public interface IProductDao {
-<<<<<<< HEAD
 	/**
-	 * 제품 등록하는 메서드
-	 * @param productVO
+	 * Vo객체 받아 새상품 등록
+	 * 상품id, 종류, 이름, 상세설명, 가격, 사진, 상태 
+	 * @param productVo
 	 * @return
 	 */
-	public int insertProduct(ProductVO productVO);
+	public int insertProduct(ProductVO productVo); 
+	
+	/**
+	 * 수정할 상품객체 VO에서 가져와 수정
+	 * @param productVo
+	 * @return
+	 */
+	public int updateProduct(ProductVO productVo);
 	/**
 	 * 제품 삭제하는 메서드
 	 * @param product_ID
@@ -29,36 +36,6 @@ public interface IProductDao {
 	 * @return
 	 */
 	public ProductVO getProduct(String product_name);
-=======
-	
-	/**
-	 * Vo객체 받아 새상품 등록
-	 * 상품id, 종류, 이름, 상세설명, 가격, 사진, 상태 
-	 * @param productVo
-	 * @return
-	 */
-	public int insertProduct(ProductVO productVo); 
-	
-	/**
-	 * 상품id를 받아 해당상품 삭제
-	 * @param product_id
-	 * @return
-	 */
-	public int deleteProduct(String product_id);
-	
-	/**
-	 * 수정할 상품객체 VO에서 가져와 수정
-	 * @param productVo
-	 * @return
-	 */
-	public int updateProduct(ProductVO productVo);
-	
-	
-	/**
-	 * vo에 저장된 상품리스트 조회
-	 * @return
-	 */
-	public List<ProductVO>ProductList();
 	
 	/**
 	 * 상품id중복체크 , 존재하는 상품번호는 등록불가
@@ -69,6 +46,5 @@ public interface IProductDao {
 	public int getProductCount(String product_id);
 	
 	
->>>>>>> branch 'main' of https://github.com/doo7777/MIDDLEPJ.git
 	
 }
