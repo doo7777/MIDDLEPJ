3eda6f4beed4d5a3da9b567fe73255e727c3bac8
ssc <ssc@dnbook01>
2024-12-10 18:20:15 +0900

    .

--- midpro/src/main/java/Dao/ScheduleDaoImpl.java
+++ midpro/src/main/java/Dao/ScheduleDaoImpl.java
@@ -1,7 +1,22 @@
 package Dao;
 
+import java.util.List;
+
 import DaoInterface.IScheduleDao;
+import Vo.ScheduleVO;
 
 public class ScheduleDaoImpl implements IScheduleDao{
+
+	@Override
+	public ScheduleVO getSchedule(int Moive_ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ScheduleVO> getAllSchedule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
