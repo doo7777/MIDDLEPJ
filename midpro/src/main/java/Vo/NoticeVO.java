fc6f8298ae2db6eecc4eca44db63c2371c4ca5d8
ssc <ssc@dnbook01>
2024-12-11 15:50:01 +0900

    조회수 추가 int notice_view

--- midpro/src/main/java/Vo/NoticeVO.java
+++ midpro/src/main/java/Vo/NoticeVO.java
@@ -5,6 +5,7 @@ public class NoticeVO {
 	private String title;
 	private String content;
 	private String post_date;
+	private int notice_view;
 	
 	public int getNotice_id() {
 		return notice_id;
@@ -14,6 +15,12 @@ public class NoticeVO {
 	}
 	public String getTitle() {
 		return title;
+	}
+	public int getNotice_view() {
+		return notice_view;
+	}
+	public void setNotice_view(int notice_view) {
+		this.notice_view = notice_view;
 	}
 	public void setTitle(String title) {
 		this.title = title;
@@ -32,9 +39,11 @@ public class NoticeVO {
 	}
 	@Override
 	public String toString() {
-		return "Notice [notice_id=" + notice_id + ", title=" + title + ", content=" + content + ", post_date="
-				+ post_date + "]";
+		return "NoticeVO [notice_id=" + notice_id + ", title=" + title + ", content=" + content + ", post_date="
+				+ post_date + ", notice_view=" + notice_view + "]";
 	}
+	
+	
 	
 	
 	
