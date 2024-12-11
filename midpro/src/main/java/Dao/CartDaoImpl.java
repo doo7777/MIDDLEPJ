511070491fa25d4227dbe57ee20bfcd8711baba1
DESKTOP-F8B9ACS\DAM <qkrekal1389@gmail.com>
2024-12-11 09:27:30 +0900

    123123

--- midpro/src/main/java/Dao/CartDaoImpl.java
+++ midpro/src/main/java/Dao/CartDaoImpl.java
@@ -7,28 +7,5 @@ import Vo.ProductVO;
 
 public class CartDaoImpl implements ICartDao{
 
-	@Override
-	public int insertProduct(ProductVO productVO) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int deleteProduct(int product_ID) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List<ProductVO> getAllProduct() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ProductVO getProduct(String Product_name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
