50631adc4423728559a468ab2e9c0e4605106ebb
ssc <ssc@dnbook01>
2024-12-11 19:59:20 +0900

    notice 서비스,다오 작성중

--- midpro/src/main/java/Service/NoticeServiceImpl.java
+++ midpro/src/main/java/Service/NoticeServiceImpl.java
@@ -1,7 +1,40 @@
 package Service;
 
+import java.util.List;
+
 import ServiceInterface.INoticeService;
+import Vo.NoticeVO;
 
 public class NoticeServiceImpl implements INoticeService{
+
+	@Override
+	public int insertNotice(NoticeVO noticeVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteNotice(int notice_ID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateNotice(NoticeVO noticeVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<NoticeVO> getAllNotice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NoticeVO getNotice(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
