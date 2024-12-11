1080bf9954eb22f0de45cd5ca819ee0583a492f1
kim <kim@JTW96>
2024-12-11 09:26:45 +0900

    ss

--- midpro/src/main/java/Dao/LocationsDaoImpl.java
+++ midpro/src/main/java/Dao/LocationsDaoImpl.java
@@ -9,26 +9,8 @@ public class LocationsDaoImpl implements ILocationsDao{
 
 	@Override
 	public List<LocationsVO> getAllLocations() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-<<<<<<< HEAD
-=======
-	@Override
-	public int getLocationsCount(String locations_ID) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public LocationsVO getLocations(String Locations_NAME) {
-		// TODO Auto-generated m
-		
-		
-		ethod stub
-		return null;
-	}
->>>>>>> branch 'main' of https://github.com/doo7777/MIDDLEPJ.git
 
 }
--- midpro/src/main/java/Dao/NonMemberDaoImpl.java
+++ midpro/src/main/java/Dao/NonMemberDaoImpl.java
@@ -1,7 +1,40 @@
 package Dao;
 
+import java.util.List;
+
 import DaoInterface.INonMemberDao;
+import Vo.NonMemberVO;
 
 public class NonMemberDaoImpl implements INonMemberDao{
+
+	@Override
+	public int insertNonMember(NonMemberVO nonMemberVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteNonMember(String nonMember_ID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<NonMemberVO> getAllNonMember() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNonMemberCount(String nonMember_ID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public NonMemberVO getNonMember(String nonMember_ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
