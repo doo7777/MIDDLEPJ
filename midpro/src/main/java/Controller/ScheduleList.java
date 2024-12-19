8f215e49a76123609f90d798d6e126950ae7eed9
user <user@DESKTOP-JDF2E1A>
2024-12-19 09:08:17 +0900

    aa

--- midpro/src/main/java/Controller/ScheduleList.java
+++ midpro/src/main/java/Controller/ScheduleList.java
@@ -15,24 +15,34 @@ import Vo.ScheduleVO;
 
 @WebServlet("/scheduleList.do")
 public class ScheduleList extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		IScheduleService service = ScheduleServiceImpl.getInstance();
-		
-		List<ScheduleVO>scdList = service.getAllSchedule();
-		
-		request.setAttribute("scd", scdList);
-		
-		request.getRequestDispatcher("/schedule/ScheduleList.jsp").forward(request, response);
-	}
+   
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+      IScheduleService service = ScheduleServiceImpl.getInstance();
+      
+      List<ScheduleVO>scdList = service.getAllDetail();
+      
+        // 선택된 영화의 시작일을 JSP에 전달
+        String selectedMovieName = request.getParameter("selectedMovie");
+        if (selectedMovieName != null) {
+            for (ScheduleVO schedule : scdList) {
+                if (schedule.getMovie_name().equals(selectedMovieName)) {
+                    request.setAttribute("selectedStartDate", schedule.getStart_date());
+                    break;
+                }
+            }
+        }
+      request.setAttribute("scd", scdList);
+      
+      request.getRequestDispatcher("/schedule/ScheduleList.jsp").forward(request, response);
+   }
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doGet(request, response);
+   }
 
 }
